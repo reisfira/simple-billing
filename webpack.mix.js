cc95735f73d68e8b9da51d1e9c472ac6c97bf939
Galuf <mohamadyuzrie@gmail.com>
2022-11-28 15:01:34 +0800

    ep 5.1 - adding select2 locally

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
+// datatables
 mix.copy('node_modules/datatables.net/js/jquery.dataTables.min.js', 'public/js/jquery.datatables.min.js');
 mix.copy('node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js', 'public/js/datatables.bootstrap4.min.js');
 mix.copy('node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css', 'public/css/datatables.bootstrap4.min.css');
+
+// select2
+mix.copy('node_modules/select2/dist/css/select2.min.css', 'public/css/select2.min.css');
+mix.copy('node_modules/@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.min.css', 'public/css/select2-bootstrap4.min.css');
+mix.copy('node_modules/select2/dist/js/select2.min.js', 'public/js/select2.min.js');
+mix.copy('node_modules/select2/dist/js/select2.full.min.js', 'public/js/select2.full.min.js');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
